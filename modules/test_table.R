c4fc3be02d8239bc230db638091afdaf22c21f6a
cyyllam <cyyllam@gmail.com>
2023-12-14 13:50:40 -0800

    revised ggplot

--- modules/test_table.R
+++ modules/test_table.R
@@ -1,14 +1,10 @@
 url <- "https://data.seattle.gov/resource/76t5-zqzr.json" # building permits
 df <- read.socrata(url, app_token = Sys.getenv("RSOCRATA_SEATTLE_APP_TOKEN"))
 
-# df2 <- df %>%
-#   mutate(issued_date2 = ymd(issueddate)) %>%
-#   mutate(issued_year = year(issued_date2)) %>%
-#   filter(issued_year >= 2022) %>%
-#   mutate(difftime = as.numeric(difftime(ymd(),
-#                                           ymd(ExpectedDate), units = "days")))
+permit_22_23<-permit_sf
+st_geometry(permit_22_23) <- NULL
 
-permit_22_23<-permit_sf %>% 
+permit_22_23_2<-permit_22_23 %>% 
   mutate(year=lubridate::year(date(issueddate)),
          housingunitsadded=as.numeric(housingunitsadded))%>%
   filter(!is.na(GEOID),
@@ -20,10 +16,12 @@ permit_22_23<-permit_sf %>%
                                     housingunitsadded>1 & housingunitsadded<=9~ '2-9 units',
                                     housingunitsadded>9 & housingunitsadded<=20 ~ '10-20 units',
                                     housingunitsadded>20 ~ 'More than 20 units')) %>%
-  group_by(GEOID, TRACT, housingunitgrp)%>%
+  group_by(GEOID, TRACT, housingunitgrp, COMPOSITE_QUINTILE)%>%
   summarise(n_permit_issued = n(),
             median_time_to_permit=median(time_to_permit), 
             mean_time_to_permit=mean(time_to_permit), 
-            sd_time_to_permit=sd(time_to_permit))%>%drop_na()%>%
+            sd_time_to_permit=sd(time_to_permit))%>%
   mutate(housingunitgrp_fact=factor(housingunitgrp, levels=c('Single-Family','2-9 units', '10-20 units', 'More than 20 units')))%>%
   mutate(COMPOSITE_QUINTILE_fact=factor(COMPOSITE_QUINTILE, levels= c('Lowest', 'Second Lowest', 'Middle', 'Second Highest', 'Highest Equity Priority')))
+
+permit_22_23_map <- rse_index %>% full_join(permit_22_23_2, by="GEOID")
--- server.R
+++ server.R
@@ -16,13 +16,9 @@ server <- function(input, output, session) {
   })
   
   output$plot <- renderPlot({
-    #input$unit_group
-    
-    # ggplot(data(), aes(x=housingunitgrp, y=median_time_to_permit, fill=COMPOSITE_QUINTILE)) +
-    #   geom_bar(stat = 'identity', position='dodge')
-    
-    ggplot(data(), aes(x=housingunitgrp_fact, y=median_time_to_permit, fill=COMPOSITE_QUINTILE_fact))+
-      geom_bar(stat='identity', position='dodge')
+
+    ggplot(data(), aes(x=housingunitgrp_fact, y=median_time_to_permit, fill=COMPOSITE_QUINTILE_fact))+geom_bar(stat='identity', position='dodge')+
+      labs(x="Housing Unit Group", y="Median Time to Permit")+ scale_fill_discrete(name = "Composite RSE Index")
     
     # # placeholder for chart
     # iris |> 
@@ -34,7 +30,7 @@ server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
     
-    create_map(lyr = rse_index, 
+    create_map(lyr = permit_22_23_map, 
                lyr_data_field = rse_index$COMPOSITE_SCORE, 
                legend_title = 'Racial Social Equity Index', 
                legend_subtitle='Composite Index Value', 
